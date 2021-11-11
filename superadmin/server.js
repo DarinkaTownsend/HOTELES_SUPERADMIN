798ab3bba728d81cd9a5c3e82026f16c856c6266
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-11-11 16:54:25 -0500

    cc

--- superadmin/server.js
+++ superadmin/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/superadmin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/superadmin/src'}),
+    res.sendFile('index.html', {root: 'dist/superadmin/'}),
 );
 
 // Start the app by listening on the default Heroku port
